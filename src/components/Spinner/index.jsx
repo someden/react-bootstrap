57917d4dc0196958c660a823697d6e548936d2c1
Denis <someden@ya.ru>
2018-01-12 09:36:56 +0000

    fix Spinner

--- src/components/Spinner/index.jsx
+++ src/components/Spinner/index.jsx
@@ -1,27 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
-import stylePropType from 'react-style-proptype';
 
 import Icon from '../Icon';
 
 import iconSizes from '../../utils/iconSizes';
 
-const Spinner = ({ size, inline, className, style, iconName }) => (
-  <span
-    className={cn({ 'p-4': !inline }, className)}
-    style={
-      inline
-        ? style
-        : {
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100%',
-            ...style,
-          }
-    }
-  >
+import './index.css';
+
+const Spinner = ({ size, inline, className, iconName, ...props }) => (
+  <span {...props} className={cn({ 'spinner-on-full-height': !inline }, className)}>
     <Icon name={iconName} size={size} pulse />
   </span>
 );
@@ -30,7 +18,6 @@ Spinner.propTypes = {
   size: PropTypes.oneOf(iconSizes),
   inline: PropTypes.bool,
   className: PropTypes.string,
-  style: stylePropType,
   iconName: PropTypes.string,
 };
 
@@ -38,7 +25,6 @@ Spinner.defaultProps = {
   size: '3x',
   inline: false,
   className: '',
-  style: {},
   iconName: 'spinner',
 };
 
