e61ab3b3529134cc787f4cfbb76a7741df0c6fc3
Denis <someden@ya.ru>
2018-05-31 21:52:13 +0000

    fix Select

--- src/components/Select/index.jsx
+++ src/components/Select/index.jsx
@@ -1,38 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import sizes from '../../utils/sizes';
 
-const Select = ({ items, size, className, children, ...props }) => (
-  <select {...props} className={`custom-select form-control-${size} ${className}`}>
-    {children ||
-      items.map(({ id = '', value = '', name = '', disabled = false }, index) => (
-        <option key={index} value={id || value} disabled={disabled}>
-          {name}
-        </option>
-      ))}
-  </select>
-);
+class Select extends Component {
+  static propTypes = {
+    items: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+        value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+        name: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+        disabled: PropTypes.bool,
+      })
+    ),
+    size: PropTypes.oneOf(sizes),
+    className: PropTypes.string,
+    innerRef: PropTypes.func,
+    children: PropTypes.node,
+  };
 
-Select.propTypes = {
-  items: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-      value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-      name: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-      disabled: PropTypes.bool,
-    })
-  ),
-  size: PropTypes.oneOf(sizes),
-  className: PropTypes.string,
-  children: PropTypes.node,
-};
+  static defaultProps = {
+    items: [],
+    size: 'md',
+    className: '',
+    innerRef: null,
+    children: null,
+  };
 
-Select.defaultProps = {
-  items: [],
-  size: 'md',
-  className: '',
-  children: null,
-};
+  render() {
+    const { items, size, className, innerRef, children, ...props } = this.props;
+    return (
+      <select
+        {...props}
+        ref={typeof innerRef === 'function' ? innerRef : undefined}
+        className={`custom-select form-control-${size} ${className}`}
+      >
+        {children ||
+          items.map(({ id = '', value = '', name = '', disabled = false }, index) => (
+            <option key={index} value={id || value} disabled={disabled}>
+              {name}
+            </option>
+          ))}
+      </select>
+    );
+  }
+}
 
 export default Select;
